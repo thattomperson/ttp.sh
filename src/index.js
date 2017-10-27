62603da2618eb539abb20af396623d80ebe76164
Thomas Albrighton <thomasalbrighton@gmail.com>
2017-10-27 11:15:48 +1030

    more quotes

--- src/index.js
+++ src/index.js
@@ -20,7 +20,11 @@ var q = document.getElementById('quote')
 var a = document.getElementById('author')
 var quotes = [
     ['Hello, friend. Hello, friend?', 'Elliot Alderson'],
-    ['If you pull the right strings, a puppet will dance any way you desire.', 'Whiterose']
+    ['If you pull the right strings, a puppet will dance any way you desire.', 'Whiterose'],
+    ['If you\'re seeing me, that means you boys fucked up.', 'Irving'],
+    ['Don\'t mistake my generosity for generosity.', 'Whiterose'],
+    ['Give a man a gun and he can rob a bank. Give a man a bank and he can rob the world.', 'Tyrell'],
+    ['I think about you a lot, Elliot. I think about that night. When we became gods.', 'Tyrell']
 ];
 
 var quote = quotes[Math.floor(Math.random()*quotes.length)];
