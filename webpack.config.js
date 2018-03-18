d59940d7be8ccec7e364d61db91ebc1ed7d2c8fe
Thomas Albrighton <thomasalbrighton@gmail.com>
2018-03-18 17:12:40 +1030

    new site

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  output: {
+    filename: '[name].js',
+    path: path.resolve('public'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.ejs',
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+};
