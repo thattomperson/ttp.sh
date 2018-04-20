e47e83790aa4be4c6457f4652bd8190856e2cb75
Thomas Albrighton <thomasalbrighton@gmail.com>
2018-04-21 02:01:32 +0930

    optimised images

--- webpack.config.js
+++ webpack.config.js
@@ -39,6 +39,14 @@ module.exports = {
         })
       },
       {
+        test: /\.(jpg|png|gif|svg)$/,
+        loader: "image-webpack-loader",
+        // Specify enforce: 'pre' to apply the loader
+        // before url-loader/svg-url-loader
+        // and not duplicate it in rules with them
+        enforce: "pre"
+      },
+      {
         test: /\.(jpg|png)$/,
         loader: "file-loader",
         options: {
