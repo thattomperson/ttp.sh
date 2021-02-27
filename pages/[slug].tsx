93f3c9b4106f8b6e80c9d65ff2b6f96a8d0a37be
Tom Albrighton <thomas.albrighton@four.io>
2021-02-27 22:44:51 +1030

    Move data files to fix build

--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -4,8 +4,8 @@ import Head from "next/head"
 export default function GoProject({ project }) {
   return <>
     <Head>
-      <meta name="go-import" content={`${process.env.VERCEL_URL}/${project.slug} git ${project.repo}`} />
-      <meta name="go-source" content={`${process.env.VERCEL_URL}/${project.slug} ${project.repo} ${project.repo}/tree/master{/dir} ${project.repo}/tree/master{/dir}/{file}#L{line}`} />
+      <meta name="go-import" content={`${process.env.NEXT_PUBLIC_VERCEL_URL}/${project.slug} git ${project.repo}`} />
+      <meta name="go-source" content={`${process.env.NEXT_PUBLIC_VERCEL_URL}/${project.slug} ${project.repo} ${project.repo}/tree/master{/dir} ${project.repo}/tree/master{/dir}/{file}#L{line}`} />
       <meta http-equiv="refresh" content={`0; url=https://pkg.go.dev/${project.repo}`} />
     </Head>
     Nothing to see here; <a href={`https://pkg.go.dev/${project.repo}`}>see the package on godoc</a>.
@@ -13,7 +13,7 @@ export default function GoProject({ project }) {
 }
 
 export async function getStaticProps({ params }) {
-  const projects = (await import('./_go_projects')).default
+  const projects = (await import('../data/_go_projects')).default
   return {
     props: {
       project: projects[params.slug]
@@ -22,7 +22,7 @@ export async function getStaticProps({ params }) {
 }
 
 export async function getStaticPaths() {
-  const projects = (await import('./_go_projects')).default
+  const projects = (await import('../data/_go_projects')).default
 
   return {
     paths: Object.keys(projects).map((slug) => ({ params: { slug }})),
