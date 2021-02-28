98198ce125ea12139a0340be44f9017b7de6a44f
Tom Albrighton <thomas.albrighton@four.io>
2021-02-28 14:22:43 +1030

    Add four logo and some animations for the card

---
+++ components/Card.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image'
+import styles from './Card.module.css'
+
+console.log(styles)
+
+interface CardProps {
+  title: string
+  image?: string
+}
+
+export default function Card({ title, image }: CardProps) {
+  return <div className={`${styles.card} rounded-xl bg-gray-800 shadow-xl flex transition-all transform hover:scale-105 relative`} style={{minHeight: '10em'}}>
+    {image ? <img className={`${styles.image} transition-all`} width="160" height="160" src={image} /> : null}
+    <div className="p-4 text-gray-200">
+      <h2 className="text-lg">{title}</h2>
+    </div>
+  </div>
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,10 @@
 import Head from 'next/head'
-
-interface CardProps {
-  title: string
-}
-
-const Card = ({ title }: CardProps) => {
-  return <div className="rounded bg-gray-800 shadow-xl p-4" style={{minHeight: '10em'}}>
-    <h1>{title}</h1>
-  </div>
-}
+import Card from '../components/Card'
 
 export default function Home({ projects }) {
   return (
-    <div className="flex flex-col lg:flex-row">
+    <>
+    <div className="flex flex-col lg:flex-row container mx-auto">
       <div className="w-full relative lg:w-1/2 flex-col flex-shrink-0 flex justify-center lg:h-screen space-y-2">
         <div className="lg:fixed p-20 w-full lg:w-1/2">
           <h1 className="bg-clip-text text-transparent bg-gradient-to-r from-orange-500 to-yellow-400 text-7xl font-bold">Hey,<br />I'm Tom</h1>
@@ -26,7 +18,7 @@ export default function Home({ projects }) {
       <div className="w-full lg:w-1/2 flex-shrink-0 p-7 space-y-4 sm:space-y-0 lg:space-y-4 sm:space-x-4 lg:space-x-0 flex flex-col sm:flex-row lg:flex-col">
         <div className="w-full space-y-4">
           <h2 className="text-gray-300 text-2xl">Work</h2>
-          <Card title="FOUR" />
+          <Card title="FOUR" image="/work/four.png" />
           <Card title="DPS" />
         </div>
         <div className="w-full space-y-4">
@@ -35,6 +27,10 @@ export default function Home({ projects }) {
         </div>
       </div>
     </div>
+    <div className="bg-gray-900 h-16 text-gray-200 text-center">
+      <span>Designed and Built by Thomas Albrighton</span>
+    </div>
+    </>
   )
 }
 
